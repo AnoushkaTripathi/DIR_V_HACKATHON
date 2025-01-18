4369f4e1c8889a7f47162f916e06283e278c5737
Sandeepan Roy <44930092+Sandeepan26@users.noreply.github.com>
2025-01-19 03:36:13 +0530

    Update processing_element.v

--- processing_element.v
+++ processing_element.v
@@ -1,11 +1,17 @@
 
-module processing_element #(parameter vector_size = 8) (input [(vector_size-1) : 0] x_in, y_in,  output [(vector_size-1):0] x_out , y_out);
+module processing_element #(parameter vector_size = 8) (input clk, reset, [(vector_size-1) : 0] x_in, y_in,  output reg [(vector_size-1):0] x_out , y_out);
   
-  reg [(vector_size - 1) : 0] weight_matrix;
-  assign x_out = x_in ; //passing data to the next processing element
+  reg [(vector_size - 1) : 0] weight_matrix = 1;
   
   //compute the partial sum
-  
-   assign y_out = weight_matrix * x_in + y_in;
+  always @(posedge clk or negedge reset) begin
+    if(!reset) begin
+    	{x_out, y_out} <= 0;
+    end
+    else begin
+    	x_out <= x_in;
+    	y_out <= weight_matrix * x_in + y_in;
+    end
+  end
   
 endmodule
